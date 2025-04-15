43cfc3245b09298ca4daf559432d16713c7ece36
Scietech Joe <scietechjoe@ScietechJoe>
2025-04-15 17:11:28 +0300

    Completed the exercise

--- game.py
+++ game.py
@@ -12,12 +12,31 @@ def number_guessing_game():
         secret_number = random.randint(1, 10)
         
         # TODO: Get player's guess (handle non-number inputs)
-        guess = None
+                # the try except is ment to give the ValueError if the user enters something that is not a number
+        try:
+            # defined remaining time and made an output
+            remaining_time = int(end_time - time.time())
+            print(f"Guess the number (1-10). Time left: {remaining_time} seconds")
+            guess = int(input("Your guess: "))
+
+            # avoids input of numbers less than 1 and those more than 10
+            if guess < 1 or guess > 10:
+                print("Please enter a number between 1 and 10.")
+                continue
+
+            # if the number guessed same as the randomly generated number then it is correct and score should be added by 1
+            elif guess == secret_number:
+                print("Correct!")
+                score += 1
+
+            # if there is no break in the above the else runs an output of wrong number and gives what the correct number was
+            else:
+                print(f"Wrong! The number was {secret_number}.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
         
-        # TODO: Check if guess is correct and update score
-        
-        # TODO: Display remaining time
-        
+
     print(f"\nTime's up! Your score: {score}")
 
 if __name__ == "__main__":
